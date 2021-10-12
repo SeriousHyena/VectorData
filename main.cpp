edeb9988f2b85101a63c461afcff604703048902
SeriousHyena <emackiii@gmail.com>
2021-10-12 06:21:36 -0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+
+
+using namespace std;
+
+vector<int> myVector;	//create a vector array object
+
+int main()
+{
+
+	return 0;
+}
