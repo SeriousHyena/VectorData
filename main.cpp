3f3c649264fc747b44a3f91b2357af70eeb43c8a
SeriousHyena <emackiii@gmail.com>
2021-10-12 06:28:13 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+/*
+
+SENTINAL-CONTROLLED REPETION AND VECTOR ARRAYS
+-> get keyboard input non-negative integers from 0 and greater
+-> accept any arbitrary number of integers from a loop and store them in a vector
+-> end the loop when a negative number is entered
+-> print twice the value of the elements in the vector
+-> use a "priming read" before the while loop
+
+*/
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,7 +17,7 @@
 
 using namespace std;
 
-vector<int> myVector;	//create a vector array object
+vector<int> myVector;	//create a vector array object with no size initialization
 
 int main()
 {
